690fb9278ededdf80e3920f1e046bc67dec433f1
Gecko <91030858+Geckomacabre@users.noreply.github.com>
2026-01-18 23:17:48 -0500

    Modify fxmanifest.lua for version and formatting
    Updated version number and changed quotes to single quotes.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,18 +1,24 @@
-fx_version "cerulean"
-resource_manifest_version "05cfa83c-a124-4cfa-a768-c24a5811d8f9"
-game "gta5"
-author "Fenix"
-description "Police"
-version "1.0"
+fx_version 'cerulean'
+game 'gta5'
 
+author 'Fenix'
+description 'Police'
+version '1.0.0'
+
+-- IMPORTANT: config MUST load first so Config table exists everywhere
 shared_scripts {
     'config.lua'
 }
 
 client_scripts {
+    '@es_extended/locale.lua',     -- Support ESX Legacy (only loads if ESX is started before)
+    '@qb-core/shared/locale.lua',  -- Support QBCore (only loads if QBCore is started)
     'client/client.lua'
 }
 
 server_scripts {
     'server/server.lua'
 }
+
+-- Optional: enable this if you use lua 5.4 features
+-- lua54 'yes'
